d2f069991c2470edf0d3eaca0242bc70a2226e04
raxracks <multisniperism@gmail.com>
2020-10-28 07:36:25 +1300

    Heroku

---
+++ public/script.js
@@ -0,0 +1,80 @@
+let embed = true;
+
+function loadPage(path) {
+  window.history.pushState(path, path, path);
+  let t = path.split("/").join("");
+  document.title = "IMH · " + t[0].toUpperCase() + t.slice(1).toLowerCase();
+  if (localStorage[path]) {
+    checkCache(path);
+    document.body.innerHTML = localStorage[path];
+    if(document.getElementsByTagName("spa").length !== 0) {
+      if(document.getElementsByTagName("spa").item(0).onload !== null) document.getElementsByTagName("spa").item(0).onload();
+      return;
+    }
+  }
+
+  fixedFetch(path + "SPA").then(function(response) {
+    response.text().then((text) => {
+      localStorage.setItem(path, text);
+      localStorage.setItem(path + "-loaded", new Date().toJSON())
+      document.body.innerHTML = text;
+      if(document.getElementsByTagName("spa").item(0).onload !== null) document.getElementsByTagName("spa").item(0).onload();
+      return;
+    });
+  });
+}
+
+function checkCache(path) {
+  fixedFetch("/check-ver/" + path + "SPA/" + localStorage[path + "-loaded"]).then(function(response) {
+    response.text().then((text) => {
+      if(text == 'true') {
+        localStorage.removeItem(path);
+        localStorage.removeItem(path + "-loaded");
+      }
+    });
+  });
+}
+
+function fixedFetch(url) {
+  if(fetch !== null) {
+    return fetch(url);
+  } 
+  
+  return new Promise(function(resolve) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        xhttp.text = function() {
+          return new Promise(function(resolve) {
+            resolve(xhttp.responseText);
+          });
+        }
+        resolve(xhttp);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  });
+}
+
+function saveConfig() {
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  var blob = new Blob([document.getElementsByClassName("json").item(0).innerText])
+  var url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = "IMH-config.sxcu";
+  a.click();
+  window.URL.revokeObjectURL(url);
+};
+
+function loadConfig() {
+  document.getElementById("URL").innerText = '"https://' + document.location.host + '/$json:data.link$"';
+  document.getElementById("requestURL").innerText = '"https://imh-host.glitch.me/upload?embed=' + embed + '"';
+}
+
+function toggleEmbed() {
+  embed = document.getElementById("embed").checked;
+  loadConfig();
+}
---
+++ server.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const request = require('request').defaults({ encoding: null });
+
+function generateBase64Image(contentType, base64) {
+  return "data:" + contentType + ";base64," + base64;
+}
+
+function generateEmbedMeta(path, image) {
+  return `
+        <!-- Primary Meta Tags -->
+        <title>IMH</title>
+        <meta name="title" content="IMH">
+        <meta name="description" content="Uploaded with IMH">
+
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://imh.glitch.me/${path}">
+        <meta property="og:title" content="${path.split("/").join("")}">
+        <meta property="og:description" content="Uploaded with IMH">
+        <meta property="og:image" content="https://i.imgur.com/${path}">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://imh.glitch.me/${path}">
+        <meta property="twitter:title" content="${path.split("/").join("")}">
+        <meta property="twitter:description" content="Uploaded with IMH">
+        <meta property="twitter:image" content="https://i.imgur.com/${path}">
+
+        <img src="${image}">`;
+};
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/config", (req, res) => {
+  res.sendFile(__dirname + "/views/config.html");
+});
+
+app.get("/extension", (req, res) => {
+  res.sendFile(__dirname + "/views/extension.html");
+});
+
+app.get("/discord", (req, res) => {
+  res.sendFile(__dirname + "/views/discord.html");
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(__dirname + "/views/upload.html");
+});
+
+app.get("/homeSPA", (req, res) => {
+  fs.readFile(__dirname + "/views/homeSPA.html", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    res.send(content);
+  });
+});
+
+app.get("/configSPA", (req, res) => {
+  fs.readFile(__dirname + "/views/configSPA.html", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    res.send(content);
+  });
+});
+
+app.get("/extensionSPA", (req, res) => {
+  fs.readFile(__dirname + "/views/extensionSPA.html", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    res.send(content);
+  });
+});
+
+app.get("/discordSPA", (req, res) => {
+  fs.readFile(__dirname + "/views/discordSPA.html", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    res.send(content);
+  });
+});
+
+app.get("/uploadSPA", (req, res) => {
+  fs.readFile(__dirname + "/views/uploadSPA.html", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    res.send(content);
+  });
+});
+
+app.get('/check-ver/:path/:version', (req, res) => {
+  let path = req.params.path;
+  let version = new Date(req.params.version);
+  
+  fs.stat(__dirname + "/views/" + path + ".html", function(err, stats) {
+    var mtime = new Date(stats.mtime);
+    res.send(mtime > version);
+  });
+});
+
+app.get('/IMH_Extension', (req, res) => {
+  res.sendFile(__dirname + "/extension/extension.zip");
+});
+
+app.get("/*", (req, res) => {
+  let path = req.path;
+  let embed = false;
+  if(path.startsWith("/i/")) {
+    path = path.split("/i/").join("");
+    embed = true;
+  }
+  request.get(`https://i.imgur.com/${path}`, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let base64 = Buffer.from(body).toString('base64');
+      let image = generateBase64Image(response.headers["content-type"], base64);
+      let imageBuffer = Buffer.from(base64, 'base64');
+      if(embed) {
+        res.writeHead(200, {
+          'Content-Type': 'image/png'
+        });
+        res.end(imageBuffer);
+      } else {
+        res.send(generateEmbedMeta(path, image));
+      };
+    };
+  });
+});
+
+app.listen(process.env.PORT, () => console.log("IMH Middleman Online!"));
+
